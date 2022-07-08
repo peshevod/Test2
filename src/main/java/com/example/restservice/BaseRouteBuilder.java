1e0a93dde747d2eaea93c51661e7b8855d74487c
Ilya Shugalev <ilya@shugalev.com>
2022-07-08 18:34:49 +0300

    First Success

--- src/main/java/com/example/restservice/BaseRouteBuilder.java
+++ src/main/java/com/example/restservice/BaseRouteBuilder.java
@@ -1,2 +1,54 @@
-package com.example.restservice;public class BaseRouteBuilder {
+package com.example.restservice;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.bean.validator.BeanValidationException;
+import org.springframework.http.MediaType;
+
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class BaseRouteBuilder extends RouteBuilder
+        {
+
+    @Override
+    public void configure() throws Exception {
+        onException(BeanValidationException.class).handled(true).process(new Processor() {
+
+            @Override
+            public void process(Exchange exchange) throws Exception {
+                Throwable cause = exchange.getProperty(Exchange.EXCEPTION_CAUGHT, Throwable.class);
+
+                exchange.getMessage().setHeader(Exchange.HTTP_RESPONSE_CODE, 400);
+                exchange.getMessage().setHeader(Exchange.CONTENT_TYPE, MediaType.APPLICATION_JSON);
+                exchange.getMessage().setBody("{error:" + cause.getMessage() + "}");
+
+            }
+        });
+
+/*        onException(InvalidRequestException.class).handled(true).process(new Processor() {
+
+            @Override
+            public void process(Exchange exchange) throws Exception {
+                Throwable cause = exchange.getProperty(Exchange.EXCEPTION_CAUGHT, Throwable.class);
+                exchange.getMessage().setHeader(Exchange.HTTP_RESPONSE_CODE, 400);
+                exchange.getMessage().setHeader(Exchange.CONTENT_TYPE, MediaType.APPLICATION_JSON);
+                exchange.getMessage().setBody("{error:" + cause.getMessage() + "}");
+
+            }
+        });*/
+
+        onException(Exception.class).handled(true).process(new Processor() {
+
+            @Override
+            public void process(Exchange exchange) throws Exception {
+                Throwable cause = exchange.getProperty(Exchange.EXCEPTION_CAUGHT, Throwable.class);
+                exchange.getMessage().setHeader(Exchange.HTTP_RESPONSE_CODE, 500);
+                exchange.getMessage().setHeader(Exchange.CONTENT_TYPE, MediaType.APPLICATION_JSON);
+                exchange.getMessage().setBody("{error:" + cause.getMessage() + "}");
+
+            }
+        });
+    }
 }
--- src/main/java/com/example/restservice/HttpRouteBuilder.java
+++ src/main/java/com/example/restservice/HttpRouteBuilder.java
@@ -1,2 +1,37 @@
-package com.example.restservice;public class HttpRouteBuilder {
+package com.example.restservice;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class HttpRouteBuilder extends BaseRouteBuilder
+{
+    @Override
+    public void configure() throws Exception {
+        super.configure();
+
+        CamelContext context = new DefaultCamelContext();
+        // it tells Camel how to configure the REST service
+        restConfiguration()
+                // Use the 'servlet' component.
+                // This tells Camel to create and use a Servlet to 'host' the RESTful API.
+                // Since we're using Spring Boot, the default servlet container is Tomcat.
+                .component("servlet")
+                // Allow Camel to try to marshal/unmarshal between Java objects and JSON
+                .bindingMode(RestBindingMode.auto);
+
+        rest().get("/kyc").route()
+//                .process("httpRequestProcessor")
+                .to("log:?level=ERROR&showBody=true").endRest();
+
+     //   context.start();
+
+//        rest().post("/kyc").type(RequestObject.class).route().to("bean-validator:myvalidatorname")
+ //               .process("httpRequestProcessor").to("log:?level=INFO&showBody=true");
+
+    }
+
 }
